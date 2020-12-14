6d2352e08a220db24629b941890288b16b2385d3
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-12-14 17:17:26 -0300

    add cors

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const path = require('path');
+const cors = require('cors');
 const app = express();
 const Axios = require('axios')
 const cookieParser = require('cookie-parser')
